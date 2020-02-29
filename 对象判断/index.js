e47ab1825653aaaf238bba3f8f0ea0b839ec7e6a
孙宇 <oncwnuA5Ebz3F15U7KIwQMAuUJTM@git.weixin.qq.com>
2020-02-29 12:03:46 +0800

    modifyFiles

--- 对象判断/index.js
+++ 对象判断/index.js
@@ -1,4 +1,4 @@
-function Whatclass() { };
+function Whatclass () { };
 var whatcalss = new Whatclass();
 var sum = function () { };
 var obj = {};
@@ -10,3 +10,9 @@ console.log(typeof sum);//function
 console.log(Object.prototype.toString.call(sum));//[object Function]
 console.log(Array.isArray([]));//true
 console.log(obj instanceof Object);//true
+
+var obj1 = undefined;
+var obj2 = null;
+console.log(obj1 instanceof Object);//false
+console.log(obj2 instanceof Object);//false
+// 因为它们都是值类型的并且没有原型
