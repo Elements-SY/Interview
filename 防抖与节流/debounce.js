607b304c69a8d546a020837f5ac45c0a97ff11c3
Elements-SY <cia1068@hotmail.com>
2020-03-12 12:20:12 +0800

    modify

--- 防抖与节流/debounce.js
+++ 防抖与节流/debounce.js
@@ -1,19 +1,23 @@
 // 防抖 在连续事件完全触发完成之后等待时间过去之后才会执行，
 // 也就是说必须等待到达所定的时间才会执行仅仅只会调用一次
+var el = document.getElementById('input')
+// 防抖事件
 function debounce (fn) {
-  var time = 1000;
-  var timeout = null;
-  return function () {
-    if (timeout !== null) {
-      clearTimeout(timeout);
+  var timer = null;
+  return function (args) { // args防抖函数的回调函数的传参
+    if (timer !== null) {
+      clearTimeout(timer);
     }
-    console.log(timeout);
-    timeout = setTimeout(fn, time);
+    timer = setTimeout(function () {
+      fn(args)
+    }, 1500);
   }
 }
-// 处理函数
-function handler () {
-  console.log(Math.random());
+// 防抖函数的回调函数
+function input (value) {
+  console.log(`${value}`)
 }
-// 滚动事件
-window.addEventListener('scroll', debounce(handler));
+var debounceInput = debounce(input)
+el.addEventListener("keyup", function (e) {
+  debounceInput(e.target.value)
+})
--- 防抖与节流/throttle.js
+++ 防抖与节流/throttle.js
@@ -1,22 +1,23 @@
 function input (value) {
-  console.log(`输入的内容${value}`)
+  console.log(`${value}`)
 }
 const ipt = document.getElementById('input')
 // 节流 在一定的时间内会执行一次
-function throttle (func, wait) {
+function throttle (func) {
+  let timer = 1000
   let lastTime = null
   // 为了避免每次调用lastTime都被清空，
   // 利用js的闭包返回一个function确保不生命全局变量也可以
-  return function () {
+  return function (args) {
     let now = new Date()
     // 如果上次执行的时间和这次触发的时间大于一个执行周期，则执行
-    if (now - lastTime - wait > 0) {
-      func()
+    if (now - lastTime - timer > 0) {
+      func(args)
       lastTime = now
     }
   }
 };
-const throttleInput = throttle(input, 500)
+const throttleInput = throttle(input)
 ipt.addEventListener("keyup", function (e) {
   throttleInput(e.target.value)
 })
