a79d75669a6bc8822364aa3514c4ebcf137809c5
Elements-SY <cia1068@hotmail.com>
2020-03-29 18:11:57 +0800

    modify

---
+++ var与let和const/index.js
@@ -0,0 +1,24 @@
+// var a = 1;
+// var a = 2;
+
+// let b = 1;
+// let b = 2;
+
+// let不可重复定义同一个变量
+
+// c = 3;
+// var c;
+
+// d = 2
+// let d;
+// 不会变量提升
+
+// const num = '张三'
+// num = '李四'
+// // 如果一个变量的值是一个基本数据类型的，那么不可修改变量的值
+
+const obj = {
+  name: '王五'
+}
+obj.name = '马六'
+console.log(obj.name)
